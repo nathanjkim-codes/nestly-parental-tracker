b86ca7bd7b0af0da508ae8f2ff2aa7cb1d0f9bf2
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-03-11 01:13:40 -0700

    feat: add dynamic name input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 const dashboard = document.getElementById("dashboard"); // Main dashboard container
-const addBtn = document.querySelector(".addBtn"); // Button to add new child
+const addBtn = document.querySelector(".add-btn"); // Button to add new child
+const childList = document.getElementById("child-list"); // Container for displaying child card
 
 let children = []; // Array to store child cards
 let selectedCard = null; // Current selected card
+
+addBtn.addEventListener("click", (e) => {
+  const inputWrapper = document.createElement("div");
+  inputWrapper.classList.add("child-input-wapper");
+
+  const nameInput = document.createElement("input");
+  nameInput.type = "text";
+  nameInput.name = "name[]";
+  nameInput.placeholder = "Enter Name";
+  nameInput.classList.add("name-input");
+
+  inputWrapper.appendChild(nameInput);
+  childList.appendChild(inputWrapper);
+});
