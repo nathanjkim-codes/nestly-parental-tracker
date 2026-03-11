5535cdf3745692d4c20ed8ffe38cec5ecf35dc2d
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-03-10 23:36:35 -0700

    chore: innitalize JS variables for dashboard and add button

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const dashboard = document.getElementById("dashboard"); // Main dashboard container
+const addBtn = document.querySelector(".addBtn"); // Button to add new child
+
+let children = []; // Array to store child cards
+let selectedCard = null; // Current selected card
