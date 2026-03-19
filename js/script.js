f73a9c3ef813681aa373be3ca0a3dedbc325103a
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-03-19 00:04:14 -0700

    wip: extract createInputWrapper function and start refactoring input flow

--- js/script.js
+++ js/script.js
@@ -17,24 +17,24 @@ function createChild(name, birth, gender) {
     growthRecords: [],
   };
 }
+function createInputWrapper() {
+  const inputWrapper = document.createElement("div");
+  const nameInput = document.createElement("input");
+  const dobInput = document.createElement("dob-input");
+  const genderSelect = document.createElement("gender-select");
+
+  inputWrapper.classList.add("input-wrapper");
+  nameInput.classList.add("name-input");
+  dobInput.classList.add("dob-input");
+  genderSelect.classList.add("gender-select");
+
+  inputWrapper.append(nameInput, dobInput, genderSelect_);
+  return { inputWrapper, nameInput, dobInput, genderSelect };
+}
 
 // Functionality: Add child input fields on button click
 addBtn.addEventListener("click", (e) => {
   //
-  function createInputWrapper() {
-    const inputWrapper = document.createElement("div");
-    const nameInput = document.createElement("input");
-    const dobInput = document.createElement("dob-input");
-    const genderSelect = document.createElement("gender-select");
-
-    inputWrapper.classList.add("input-wrapper");
-    nameInput.classList.add("name-input");
-    dobInput.classList.add("dob-input");
-    genderSelect.classList.add("gender-select");
-
-    inputWrapper.append(nameInput, dobInput, genderSelect_);
-    return { inputWrapper, nameInput, dobInput, genderSelect };
-  }
 
   // Add input wrapper to the child list
   childList.appendChild(inputWrapper);
