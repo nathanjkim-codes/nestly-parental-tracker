5255cd2f0f7390d1acc479615b13cd53807506fe
Jung hwan Kim (Nathan) <nathanjungkim.dev@gmail.com>
2026-03-27 14:11:54 -0700

    refactor: move DOM elements from script.js to main.js for modular structure

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 import { createChildCard } from "./ui.js";
 import { children, createChild } from "./data.js";
+
+// Main UI elements
+const dashboard = document.getElementById("dashboard"); // Main dashboard container
+const addBtn = document.querySelector(".add-btn"); // Button to add new child
+const childList = document.getElementById("child-list"); // Container for displaying child card
+
+const childName = document.querySelector(".child-name");
+const childBirth = document.querySelector(".child-birth");
+const childHeight = document.querySelector(".child-height");
+const childWeight = document.querySelector(".child-weight");
+const childGender = document.querySelector(".child-gender");
+
+const editBtn = document.querySelector(".edit-btn");
+const deleteBtn = document.querySelector(".delete-btn");
